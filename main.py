49525c13f3e239dd3d12c4c5e31fe2090861c2c2
Martin Pražák <martin.prazak@lutherone.com>
2023-08-12 03:09:04 +0200

    Added surrounding calculations

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-WIDTH = 1
-HEIGHT = 1
-STATES = 3
+from dataclasses import dataclass
+from enum import IntEnum
+
+WIDTH = 3
+HEIGHT = 3
+STATES = 2
 WRAP = False
-DIAGONAL = True
+DIAGONAL = False
+INDEX_CACHE: dict[int, list[int, None]] = {}
+
+State = IntEnum('States', dict((str(i), i) for i in range(STATES)))
+
+
+@dataclass
+class RuleCondition:
+    appearances: int
+    state: State
+
+
+@dataclass
+class Rule:
+    state: State
+    conditions: list[RuleCondition]
 
 
 class Grid:
-    rules: list[...]
-    data: list[int]
+    rules: list[Rule]
+    data: list[State]
     states: int
     width: int
     height: int
@@ -16,7 +34,7 @@ class Grid:
 
     def __init__(
             self,
-            rules: list[...],
+            rules: list[Rule],
             signature: int,
             states: int,
             width: int,
@@ -24,7 +42,19 @@ class Grid:
             wrap: bool = True,
             diagonal: bool = True
     ):
-        self.rules = rules
+        if len(rules) == 0:
+            self.rules = [
+                Rule(
+                    State['0'],
+                    [
+                        RuleCondition(neighbor, State[str(state)])
+                        for state in range(states)
+                        for neighbor in range(8)
+                    ]
+                )
+            ]
+        else:
+            self.rules = rules
         self.states = states
         self.width = width
         self.height = height
@@ -32,30 +62,131 @@ class Grid:
         self.diagonal = diagonal
         self.data = convert_base(signature, states)
         for _ in range((width * height) - len(self.data)):
-            self.data.insert(0, 0)
+            self.data.insert(0, State['0'])
 
     def signature(self) -> int:
-        return int(''.join(map(lambda i: str(i), self.data)), self.states)
+        return int(''.join(map(lambda i: str(i.value), self.data)), self.states)
 
-    def get_surrounding(self, index: int) -> dict[int, int]:
-        res = {}
-        # TODO: implement surroundings
+    def calculate_non_wrapping_non_diagonal(self, index: int) -> list[int]:
+        cached = None
+        if index in INDEX_CACHE:
+            cached = INDEX_CACHE[index]
+        if cached is not None:
+            return [item for item in [cached[1], cached[3], cached[5], cached[7]] if item is not None]
+        neighbors = []
+        cache: list[int | None] = [None]
+        if (index - 1) >= 0:
+            neighbors.append(index - 1)
+            cache.append(index - 1)
+        else:
+            cache.append(None)
+        cache.append(None)
+        if (index + 1) < len(self.data):
+            neighbors.append(index + 1)
+            cache.append(index + 1)
+        else:
+            cache.append(None)
+        cache.append(None)
+        if (index - self.width) >= 0:
+            neighbors.append(index - self.width)
+            cache.append(index - self.width)
+        else:
+            cache.append(None)
+        if (index + self.width) < len(self.data):
+            neighbors.append(index + self.width)
+            cache.append(index + self.width)
+        else:
+            cache.append(None)
+        cache.append(None)
+        INDEX_CACHE[index] = cache
+        return neighbors
+
+    def calculate_wrapping_non_diagonal(self, index: int) -> list[int]:
+        # TODO: add caching
+        neighbors = []
+        if (index - 1) >= 0:
+            neighbors.append(index - 1)
+        else:
+            neighbors.append(index - 1 + self.width)
+        if (index + 1) < len(self.data):
+            neighbors.append(index + 1)
+        else:
+            neighbors.append(index + 1 - self.width)
+        if (index - self.width) >= 0:
+            neighbors.append(index - self.width)
+        else:
+            neighbors.append(index - self.width + (self.height * self.width))
+        if (index + self.width) < len(self.data):
+            neighbors.append(index + self.width)
+        else:
+            neighbors.append(index + self.width - (self.height * self.width))
+        return neighbors
+
+    def calculate_non_wrapping_diagonal(self, index: int) -> list[int]:
+        # TODO:
+        #  add caching
+        #  be careful, we might have already used cache for the non diagonal ones, we need to take that into account
+        #  cache might need to be updated
+        neighbors = self.calculate_non_wrapping_non_diagonal(index)
+        if (index - self.width - 1) >= 0:
+            neighbors.append(index - self.width - 1)
+        if (index - self.width + 1) >= 0:
+            neighbors.append(index - self.width + 1)
+        if (index + self.width - 1) < len(self.data):
+            neighbors.append(index + self.width - 1)
+        if (index + self.width + 1) < len(self.data):
+            neighbors.append(index + self.width + 1)
+        return neighbors
+
+    def calculate_wrapping_diagonal(self, index: int) -> list[int]:
+        # TODO:
+        #  add caching
+        #  be careful, we might have already used cache for the non diagonal ones, we need to take that into account
+        #  cache might need to be updated
+        neighbors = self.calculate_wrapping_non_diagonal(index)
+        if (index - self.width - 1) >= 0:
+            neighbors.append(index - self.width - 1)
+        else:
+            neighbors.append(index - self.width - 1 + (self.height * self.width))
+        if (index - self.width + 1) >= 0:
+            neighbors.append(index - self.width + 1)
+        else:
+            neighbors.append(index - self.width + 1 + (self.height * self.width))
+        if (index + self.width - 1) < len(self.data):
+            neighbors.append(index + self.width - 1)
+        else:
+            neighbors.append(index + self.width - 1 - (self.height * self.width))
+        if (index + self.width + 1) < len(self.data):
+            neighbors.append(index + self.width + 1)
+        else:
+            neighbors.append(index + self.width + 1 - (self.height * self.width))
+        return neighbors
+
+    def get_surrounding(self, index: int) -> list[RuleCondition]:
+        neighbors = []
         match [self.wrap, self.diagonal]:
             case [False, False]:
-                ...
-            case [False, True]:
-                ...
+                neighbors = self.calculate_non_wrapping_non_diagonal(index)
             case [True, False]:
-                ...
+                neighbors = self.calculate_wrapping_non_diagonal(index)
+            case [False, True]:
+                neighbors = self.calculate_non_wrapping_diagonal(index)
             case [True, True]:
-                ...
-        return res
+                neighbors = self.calculate_wrapping_diagonal(index)
+        result = []
+        for state in range(self.states):
+            result.append(RuleCondition(0, State[str(state)]))
+        for neighbor in neighbors:
+            state = self.data[neighbor]
+            result[int(state)].appearances += 1
+        return result
 
-    def apply_rules(self, cell: int, neighborhood: dict[int, int]) -> int:
-        # TODO: implement rules
-        match self.rules:
-            case _:
-                return 0
+    def apply_rules(self, cell: State, neighborhood: list[RuleCondition]) -> State:
+        # TODO:
+        #  deal with this, unsure which condition should take priority
+        #  maybe the first one?
+        for rule in self.rules:
+            return rule.state
 
     def generate_next_grid(self) -> 'Grid':
         grid = Grid(self.rules, 0, self.states, self.width, self.height, self.wrap, self.diagonal)
@@ -77,7 +208,7 @@ class Grid:
         )
 
 
-def convert_base(value: int, base: int) -> list[int]:
+def convert_base(value: int, base: int) -> list[State]:
     result = []
     while value > 0:
         result.insert(0, value % base)
@@ -92,7 +223,7 @@ def main() -> None:
         grid = Grid([], signature, STATES, WIDTH, HEIGHT, WRAP, DIAGONAL)
         next_grid = grid.generate_next_grid()
         result_map[signature] = next_grid.signature()
-    print(f'Generated automaton map: \n  {result_map}')
+    print(f'Generated automaton map: \n  {result_map}\n')
 
 
 if __name__ == '__main__':
